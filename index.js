c056e85bbf52b7f1fab030e289293f8137558982
TymonCole <coletymon@gmail.com>
2023-11-15 19:38:50 -0600

    Complete working files for project.

---
+++ index.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".container");
+const rows = document.querySelectorAll(".row");
+
+// function randomColor() {
+//     let letters = "0123456789ABCDEF";
+//     let color = "#";
+//     for(let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+
+function createGrid(numRow, numCell) {
+    // create num of grid rows
+    for(let i = 0; i < numRow; i++) {
+        const row = document.createElement("div");
+        container.appendChild(row).classList.add("row");
+        
+        // create num of grid columns
+        for(let j = 0; j < numCell; j++) {
+            const cell = document.createElement("div");
+            row.appendChild(cell).classList.add("cell");
+
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = "#557C55";
+            });
+        }
+    }
+}
+
+function clearGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+const button = document.querySelector("button");
+button.addEventListener('click', () => {
+    let num = prompt("Enter a number of grid squares below 100");
+
+    if(num < 100) {
+        clearGrid();
+        createGrid(num, num);
+    }
+});
+
+console.log(randomColor);
